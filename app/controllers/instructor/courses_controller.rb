22ed61460218ef39b61aee0079b45d8dac84ee62
“Kareem <kareembyrd@gmail.com>
2019-09-20 19:06:48 +0000

    Lesson 14

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -7,17 +7,21 @@ class Instructor::CoursesController < ApplicationController
 	end
 
 	def create
-		@course = current_user.courses.create(course_params)
-		redirect_to instructor_course_path(@course)
+	  @course = current_user.courses.create(course_params)
+	  if @course.valid?
+	    redirect_to instructor_course_path(@course)
+	  else
+	  	render :new, status: :unprocessable_entity
+	  end
 	end
 
 	def show
-		@course = Course.find(params[:id])
+	  @course = Course.find(params[:id])
 	end
 
 	private
 
 	def course_params
-		params.require(:course).permit(:title, :description, :cost)
+	  params.require(:course).permit(:title, :description, :cost)
 	end
 end
